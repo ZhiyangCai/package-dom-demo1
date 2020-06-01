804c84421e7811a3c0309f862578bcdb2518ddd4
zhiyangcai <2313120974@qq.com>
2020-06-02 01:24:34 +0800

    封装dom

---
+++ src/main.js
@@ -0,0 +1,9 @@
+// dom.style(test, { border: "1px solid red", color: "blue" });
+// dom.style(test, "border");
+// dom.style(test, "border", "1px solid red");
+
+const div = dom.find("#test>.red")[0]; // 获取对应的元素
+dom.style(div, "color", "red"); // 设置 div.style.color
+
+const divList = dom.find(".red"); // 获取多个 div.red 元素
+dom.each(divList, (n) => console.log(n)); // 遍历 divList 里的所有元素
